8691b896009df7b7318cde76df33bf455400dac4
Wired7475FRC <wiredrobotics7475@gmail.com>
2022-09-20 11:46:40 -0400

    start configuring motor controllers

--- BitiBot/src/main/java/frc/robot/Robot.java
+++ BitiBot/src/main/java/frc/robot/Robot.java
@@ -19,23 +19,98 @@ public class Robot extends TimedRobot {
   private Joystick m_leftStick;
   private Joystick m_rightStick;
 
-  private final MotorController m_leftMotor = new PWMSparkMax(0);
-  private final MotorController m_rightMotor = new PWMSparkMax(1);
 
-  @Override
-  public void robotInit() {
-    // We need to invert one side of the drivetrain so that positive voltages
-    // result in both sides moving forward. Depending on how your robot's
-    // gearbox is constructed, you might have to invert the left side instead.
-    m_rightMotor.setInverted(true);
 
-    m_myRobot = new DifferentialDrive(m_leftMotor, m_rightMotor);
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+  public class Robot extends TimedRobot {
+  
+    private Command m_autonomousCommand;
+  
+    private RobotContainer m_robotContainer;
+  
+    String filePath = "src\\main\\java\\frc\\robot\\subsystems\\motors";
+    //motor basicTalonSRX = new motor("basicTalonSRX", filePath);
+    Motor basicTalonFX = new Motor("basicTalonFX");
+    //motor basicFalconSPX = new motor("basicFalconSPX", filePath);
+    /**
+     * This function is run when the robot is first started up and should be used for any
+     * initialization code.
+     */
+    @Override
+    public void robotInit() {
+      // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+      // autonomous chooser on the dashboard.
+      motor.setMotorConfigPath(filePath);
+    }
+  
+    /**
+     * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
+     * that you want ran during disabled, autonomous, teleoperated and test.
+     *
+     * <p>This runs after the mode specific periodic functions, but before LiveWindow and
+     * SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+      // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+      // commands, running already-scheduled commands, removing finished or interrupted commands,
+      // and running subsystem periodic() methods.  This must be called from the robot's periodic
+      // block in order for anything in the Command-based framework to work.
+      CommandScheduler.getInstance().run();
+    }
+  
+    /** This function is called once each time the robot enters Disabled mode. */
+    @Override
+    public void disabledInit() {}
+  
+    @Override
+    public void disabledPeriodic() {}
+  
+    /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
+    @Override
+    public void autonomousInit() {
+      m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+  
+      // schedule the autonomous command (example)
+      if (m_autonomousCommand != null) {
+        m_autonomousCommand.schedule();
+      }
+    }
+  
+    /** This function is called periodically during autonomous. */
+    @Override
+    public void autonomousPeriodic() {}
+  
+    @Override
+    public void teleopInit() {
+      // This makes sure that the autonomous stops running when
+      // teleop starts running. If you want the autonomous to
+      // continue until interrupted by another command, remove
+      // this line or comment it out.
+      if (m_autonomousCommand != null) {
+        m_autonomousCommand.cancel();
+      }
+    }
+  
+    /** This function is called periodically during operator control. */
+    @Override
+    public void teleopPeriodic() {
+    }
+  
+    @Override
+    public void testInit() {
+      
+  
+    }
+  
+    /** This function is called periodically during test mode. */
+    @Override
+    public void testPeriodic() {}
+  
+    /** This function is called once when the robot is first started up. */
+    @Override
+    public void simulationInit() {}
+  
+    /** This function is called periodically whilst in simulation. */
+    @Override
+    public void simulationPeriodic() {}
   }
-
-  @Override
-  public void teleopPeriodic() {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
-  }
-}
