322f03c5ae5b709080214a33fe1b6599d012e135
Wired-7475 <wiredrobotics7475@gmail.com>
2022-10-11 15:33:05 -0400

    work plz

--- code/2022/src/main/java/frc/robot/subsystems/DriveTrain.java
+++ code/2022/src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -23,32 +23,31 @@ public class DriveTrain extends SubsystemBase{
     private static double HIGH_SPEED_COEFF = 0.5;
 
     public DriveTrain() {
-        m_leftDrive = new MotorControllerGroup(leftDrive1, leftDrive2);
-        m_rightDrive = new MotorControllerGroup(rightDrive1, rightDrive2);
-        drivetrain = new DifferentialDrive(m_leftDrive,m_rightDrive);
-
         leftDrive1 = new WPI_VictorSPX(9);
         leftDrive2 = new WPI_VictorSPX(13);
         rightDrive1 = new WPI_TalonSRX(14);
         rightDrive2 = new WPI_TalonSRX(15);
 
+        m_leftDrive = new MotorControllerGroup(leftDrive1, leftDrive2);
+        m_rightDrive = new MotorControllerGroup(rightDrive1, rightDrive2);
+        drivetrain = new DifferentialDrive(m_leftDrive,m_rightDrive);
     }
 
     public void teleDrive() {
         if (OI.getDriveBumper(OI.RIGHT)){
-            xSpeed = OI.getDriveRightStick();
-            zRotation = OI.getDriveLeftStick();
+            xSpeed = OI.getDriveLeftStick();
+            zRotation = OI.getDriveRightStick();
         } else if (OI.getDriveBumper(OI.LEFT)){
-            xSpeed = OI.getDriveRightStick() * MED_SPEED_COEFF;
-            zRotation = OI.getDriveLeftStick() * MED_SPEED_COEFF;
+            xSpeed = OI.getDriveLeftStick() * MED_SPEED_COEFF;
+            zRotation = OI.getDriveRightStick() * MED_SPEED_COEFF;
         } else if (OI.getDriveTrigger(OI.RIGHT)){
-            xSpeed = OI.getDriveRightStick() *MED_SPEED_COEFF;
-            zRotation = OI.getDriveLeftStick() * LOW_SPEED_COEFF;
+            xSpeed = OI.getDriveLeftStick() *MED_SPEED_COEFF;
+            zRotation = OI.getDriveRightStick() * LOW_SPEED_COEFF;
         }  else {
-            xSpeed = OI.getDriveRightStick() * HIGH_SPEED_COEFF;
-            zRotation = OI.getDriveLeftStick() * HIGH_SPEED_COEFF;
+            xSpeed = OI.getDriveLeftStick() * HIGH_SPEED_COEFF;
+            zRotation = OI.getDriveRightStick() * HIGH_SPEED_COEFF;
         }
-        drivetrain.arcadeDrive(-xSpeed, zRotation);
+        drivetrain.arcadeDrive(xSpeed, zRotation);
         drivetrain.feed();
     }
 }
